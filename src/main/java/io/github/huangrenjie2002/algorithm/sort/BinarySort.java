382143530d60bb3ee75c48f7ccd1e572b0ba827b
HuangRenjie <huangrenjie2002@qq.com>
2024-04-10 10:52:42 +0800

    init

--- src/main/java/io/github/huangrenjie2002/algorithm/sort/BinarySort.java
+++ src/main/java/io/github/huangrenjie2002/algorithm/sort/BinarySort.java
@@ -2,7 +2,18 @@ package io.github.huangrenjie2002.algorithm.sort;
 
 public class BinarySort {
 
-    public static void sort(Object target, Object...arg){
-        
+    public static int sort(int target, int[] arg){
+        int i = 0, j = arg.length;
+        while(i < j){
+            int mid = i + (j - i) >>> 1;
+            if (target < arg[mid]){
+                j = mid;
+            }else if (arg[mid] < target){
+                i = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
